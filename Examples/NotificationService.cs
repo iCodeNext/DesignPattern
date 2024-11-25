cfe8764a8ab97b7473b0ca61967bb2f1ae02197e
MohammadKarimi <mha.karimi@gmail.com>
2024-11-25 22:50:04 +1100

    Update Example

--- Examples/NotificationService.cs
+++ Examples/NotificationService.cs
@@ -1,13 +1,14 @@
-public class NotificationService
+public class NotificationService()
 {
-    public ... CreateNotification(string type)
+    public ... Get(string type)
     {
         if (type == "Email")
             return new ...();
         else if (type == "SMS")
             return new ...();
-        else if (type == "Push")
-            return new ...();
+        //else if (type == "Push")
+        //	return new ...();
+
         else
             throw new ArgumentException("Invalid notification type.");
     }
