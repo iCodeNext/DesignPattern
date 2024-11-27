96935ca0608b6c5aaad4484b566a67d53be068cf
Mahdi <aparat1337@gmail.com>
2024-11-28 00:19:36 +0330

    E2 Changes

--- Examples/E1/NotificationService.cs
+++ Examples/E1/NotificationService.cs
@@ -1,19 +1,15 @@
-using Application.Core.Interfaces;
-using Application.Infrastructure.Services;
-using Application.Infrastructure.Services.NotificationServices;
+//public class NotificationService()
+//{
+//    public INotificationService Get(string type)
+//    {
+//        if (type == "Email")
+//            return new EmailService();
+//        else if (type == "SMS")
+//            return new SmsService();
+//        //else if (type == "Push")
+//        //	return new ...();
 
-public class NotificationService()
-{
-    public INotificationService Get(string type)
-    {
-        if (type == "Email")
-            return new EmailService();
-        else if (type == "SMS")
-            return new SmsService();
-        //else if (type == "Push")
-        //	return new ...();
-
-        else
-            throw new ArgumentException("Invalid notification type.");
-    }
-}
+//        else
+//            throw new ArgumentException("Invalid notification type.");
+//    }
+//}
