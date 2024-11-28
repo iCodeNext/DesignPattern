638f8edc70a927bac0df8b9b5898f5b6c2fb271e
FatemeYaghooblou <fateme.ygh76@gmail.com>
2024-11-28 21:51:02 +0330

    Fix Cargo Changes

---
+++ Examples/E3/Cargo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.E3
+{
+
+    public class Cargo
+    {
+        public IShippingFactory Send(string shippingType)
+        {
+            if (shippingType == "Air")
+                return new AirFactory();
+            else if (shippingType == "Ship")
+                return new ShipFactory("", "");
+            else if (shippingType == "Train")
+                return new TrainFactory();
+
+            else
+                throw new ArgumentException("Invalid Shipping type.");
+        }
+    }
+
+    public interface IShipping
+    {
+        void Send();
+    }
+
+    public class Air : IShipping
+    {
+        public void Send()
+        {
+        }
+    }
+
+
+    public class Ship : IShipping
+    {
+        private readonly string _origin;
+        private readonly string _destination;
+
+        public Ship(string origin, string destination)
+        {
+            _origin = origin;
+            _destination = destination;
+        }
+
+        public void Send()
+        {
+        }
+    }
+
+
+    public class Train : IShipping
+    {
+        public void Send()
+        {
+        }
+    }
+    public interface IShippingFactory
+    {
+        IShipping CreateShipping();
+    }
+
+    public class AirFactory : IShippingFactory
+    {
+        private static Air _air;
+
+        public IShipping CreateShipping()
+        {
+            if (_air == null)
+            {
+                _air = new Air();
+            }
+
+            return _air;
+        }
+    }
+
+    public class ShipFactory : IShippingFactory
+    {
+
+        private readonly string _origin;
+        private readonly string _destination;
+
+        public ShipFactory(string origin, string destination)
+        {
+            _origin = origin;
+            _destination = destination;
+        }
+
+        public IShipping CreateShipping()
+        {
+            return new Ship(_origin, _destination);
+        }
+
+    }
+    public class TrainFactory : IShippingFactory
+    {
+
+        public IShipping CreateShipping()
+        {
+            Train train = new Train();
+            Call();
+            return train;
+        }
+        private void Call()
+        {
+        }
+    }
+}
+
