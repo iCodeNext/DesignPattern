38f493d1d079676258c41afe0a6bf6b14a11f8f4
ramix <rambod.ghs@gmail.com>
2024-11-29 17:34:15 +0330

    - Online last session example implemented

--- Examples/E1/NotificationService.cs
+++ Examples/E1/NotificationService.cs
@@ -1,15 +1,44 @@
 public class NotificationService()
 {
-    public ... Get(string type)
+    public INotificationService Get(string type)
     {
         if (type == "Email")
-            return new ...();
-        else if (type == "SMS")
-            return new ...();
-        //else if (type == "Push")
-        //	return new ...();
+            return new EmailNotificationService();
+        if (type == "SMS")
+            return new SmsNotificationService();
+        if (type == "Push")
+            return new PushNotificationService();
 
-        else
-            throw new ArgumentException("Invalid notification type.");
+        throw new ArgumentException("Invalid notification type.");
+    }
+}
+
+
+public interface INotificationService
+{
+    void Send(string message);
+}
+
+public class EmailNotificationService : INotificationService
+{
+    public void Send(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class SmsNotificationService : INotificationService
+{
+    public void Send(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class PushNotificationService : INotificationService
+{
+    public void Send(string message)
+    {
+        throw new NotImplementedException();
     }
 }
