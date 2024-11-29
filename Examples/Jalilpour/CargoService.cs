1828e7c4f527ca6e9d21dbef2cdca8497dc7a572
Hamideh Jalilpour <j.hamide@gmail.com>
2024-11-29 12:38:01 +0330

    FactoryDesignPatternLerning_StepOne

---
+++ Examples/Jalilpour/CargoService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Jalilpour
+{
+    public class CargoService
+    {
+        public object DeliverCargo(string cargoType)
+        {
+            if (cargoType == "Air")
+                return new AirService();
+            else if (cargoType == "Ship")
+                return new TrainService();
+            else if (cargoType == "Trin")
+                return new ShipService();
+            else
+                throw new ArgumentException("Invalid Cargo Type");
+        }
+    }
+
+    public class AirService
+    {
+
+    }
+
+    public class TrainService
+    {
+
+    }
+
+    public class ShipService
+    {
+
+    }
+}
