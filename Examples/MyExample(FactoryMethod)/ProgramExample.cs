19df98cfe662b4e7807cbb9d084c333d0d2ecbfa
Rohollah <Rohollahhosseini00@gmail.com>
2024-11-29 18:15:53 +0330

    Factory Method Example

---
+++ Examples/MyExample(FactoryMethod)/ProgramExample.cs
@@ -0,0 +1,22 @@
+﻿namespace Examples.MyExample_FactoryMethod_;
+
+public class ProgramExample
+{
+    public static void RunApplication()
+    {
+
+        ITransportFactory airFactory = new AirTransportationServiceFactory();
+        ITransport airTransport = airFactory.CreateTransport();
+        airTransport.Book();
+
+
+        ITransportFactory shipFactory = new ShipTransportationServiceFactory("StartLocation", "EndLocation");
+        ITransport shipTransport = shipFactory.CreateTransport();
+        shipTransport.Book();
+
+
+        ITransportFactory trainFactory = new TrainTransportationServiceFactory();
+        ITransport trainTransport = trainFactory.CreateTransport();
+        trainTransport.Book();
+    }
+}
