d95ff34d2083997081828a786ae0b6d8731afd49
ramix <rambod.ghs@gmail.com>
2024-11-29 17:27:53 +0330

    - Common interface created - Transportations implemented

---
+++ Factory.Cargo/Common/ITransportationFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Factory.Cargo.Factory;
+
+public interface ITransportationFactory
+{
+    public ITransportation Create();
+    public ITransportation Create(string origin, string destination);
+}
