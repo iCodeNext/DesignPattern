4adeb18102167aeacfcffdd5842f8c6b51b7534c
danaei <danaee.web@gmail.com>
2024-12-08 22:55:09 +0330

    Add PaymentService

---
+++ Examples/CryptoCurrencyExternalLibrary/CryptoCurrency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCurrencyExternalLibrary
+{
+    public class CryptoCurrency
+    {
+        public decimal ExternalProcessPayment(decimal amount)
+        {
+            return amount;
+        }
+
+    }
+}
